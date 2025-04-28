b43b5f4cb2fda362aea0c0966d252a9d86c09fcc
Kirill Sirotkin <kiriyms@gmail.com>
2025-04-28 13:39:07 +0300

    refactor: remove old version files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,54 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthLoginService } from './guards/auth-login/auth-login.service';
-import { AuthJwtService } from './guards/auth-jwt/auth-jwt.service';
-import { AuthModule } from './modules/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseService } from './services/database/database.service';
-import { ReportModule } from './modules/report/report.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { AuthAlphaModule } from './modules/auth-alpha/auth-alpha.module';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { ReportGenerationService } from './services/report-generation/report-generation.service';
-import { BullModule } from '@nestjs/bullmq';
-import { ReportWorker } from './workers/report.worker';
-import { FeedbackModule } from './modules/feedback/feedback.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(), ReportModule, AuthAlphaModule, 
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '14d' },
-    }),
-    ServeStaticModule.forRoot({
-      // rootPath: join(__dirname, '..', 'reports'),
-      rootPath: join(__dirname, 'reports'),
-      serveRoot: '/reports',
-      serveStaticOptions: {
-        setHeaders: (res, path, stat) => {
-          res.setHeader('Content-Type', 'application/pdf')
-        }
-      }
-    }),
-    FeedbackModule,
-    // BullModule.forRoot({
-    //   connection: {
-    //     host: 'localhost',
-    //     port: 6379,
-    //   },
-    //   defaultJobOptions: {
-    //     attempts: 3,
-    //     delay: 1000,
-    //     removeOnComplete: 100,
-    //     removeOnFail: 100,
-    //   }
-    // }),
-  ],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, AuthLoginService, AuthJwtService, DatabaseService, ReportGenerationService, ReportWorker],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ import { NextFunction, Request, Response } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['error', 'warn', 'log', 'fatal'] });
   app.enableCors({ origin: ['http://localhost:3000', 'https://a11y-project-steel.vercel.app'] });
-  app.use(function (req: Request, res: Response, next: NextFunction) {
-    req.setTimeout(360000)
-    res.setTimeout(360000)
-    next()
-  })
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
